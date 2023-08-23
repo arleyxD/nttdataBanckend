3bc3bfbbf32395ea827d5d9a792434b6f2e1757e
arleyxD <castearley@gmail.com>
2023-08-23 12:59:45 -0500

    Creacion de proyecto basico

---
+++ NTTdata/src/main/java/com/springboot/app/NTTdata/NtTdataApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.app.NTTdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NtTdataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NtTdataApplication.class, args);
+	}
+
+}
---
+++ NTTdata/src/test/java/com/springboot/app/NTTdata/NtTdataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.app.NTTdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NtTdataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
